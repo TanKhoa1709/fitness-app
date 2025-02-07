e3c8a442dffa70e005e7416f788a0edb2557862b
TanKhoa1709 <nguyentankhoa170924@gmail.com>
2025-02-07 11:52:06 +0700

    add search bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,33 +7,67 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        elevation: 0,
-        leading: Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/Arrow - Left 2.svg',
-            width: 20,
-            height: 20,
-          ),
-        ),
-        actions: [
+      appBar: appBar(),
+      body: Column(
+        children: [
           Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withValues(alpha: 0.11),
+                  spreadRadius: 0,
+                  blurRadius: 40,
+                )
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.all(15),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 0,
+      leading: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SvgPicture.asset(
+          'assets/icons/Arrow - Left 2.svg',
+          width: 20,
+          height: 20,
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
             alignment: Alignment.center,
             margin: EdgeInsets.all(10),
             width: 37,
@@ -47,8 +81,8 @@ class HomePage extends StatelessWidget {
               height: 5,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
